89385d54eaac67b888045843b7345e1e9a27bb7a
jeroenjota <jeroen@jota.nl>
2025-12-21 11:10:53 +0100

    first setup

---
+++ src/admin/components/PhotoManager.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2 class="font-semibold mb-2">Foto’s</h2>
+
+    <input type="file" multiple @change="upload" />
+
+    <div class="grid grid-cols-4 gap-2 mt-4">
+      <div
+        v-for="foto in fotos"
+        :key="foto.id"
+        class="relative"
+      >
+        <img :src="foto.url" />
+        <button @click="remove(foto.id)">✕</button>
+      </div>
+    </div>
+  </div>
+</template>
